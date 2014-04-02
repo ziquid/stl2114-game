20612372345ceeebe3f92b1c34bc0013e9eee93b
Joseph Cheek <joseph@cheek.com>
2014-04-02 17:44:23 -0400

    minor fixes for resetting skill points

--- sites/all/modules/stlouis/stlouis_elders_ask_reset_skills.tpl.php
+++ sites/all/modules/stlouis/stlouis_elders_ask_reset_skills.tpl.php
@@ -7,19 +7,23 @@
 
   $game_user = $fetch_user();
   $fetch_header($game_user);
-  include_once(drupal_get_path('module', $game) . '/game_defs.inc');
+  include(drupal_get_path('module', $game) . '/game_defs.inc');
+  $arg2 = check_plain(arg(2));
 
   echo <<< EOF
 <div class="title">
-Do you really want to reset your skill points?
+  Do you really want to reset your skill points?
 </div>
 <div class="subtitle">
-All the $initiative, Endurance, $elocution, and Action your character has
+  All the $initiative, $endurance, $elocution, and $action your character has
   collected will be converted back into skill points
 </div>
 <div class="elders-menu big">
-<div class="menu-option"><a href="/$game/elders_do_reset_skills/$phone_id">Yes,
-  I want to reset my skill points</a></div>
+  <div class="menu-option">
+    <a href="/$game/elders_do_reset_skills/$arg2">
+      || Make it So ||
+    </a>
+  </div>
 </div>
 EOF;
     
