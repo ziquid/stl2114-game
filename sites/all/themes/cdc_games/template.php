73e2d88758f963c9d7f245df3ed0f2c889563204
Joseph Cheek <joseph@cheek.com>
2014-04-24 13:27:41 -0400

    add leaflet css and js, again

--- sites/all/themes/cdc_games/template.php
+++ sites/all/themes/cdc_games/template.php
@@ -12,3 +12,17 @@
 function cdc_games_placeholder($text) {
   return '<span class="placeholder">'. check_plain($text) .'</span>';
 }
+
+function cdc_games_preprocess_page(&$vars, $hook) {
+
+  if (arg(1) == 'move') {
+
+    $vars['head'] .= '<link '. drupal_attributes(array(
+      'rel' => 'stylesheet',
+      'type' => 'text/css',
+      'href' => 'http://cdn.leafletjs.com/leaflet-0.7.2/leaflet.css')
+    ) ." />\n";
+
+  }
+
+}
