5b2433a0843bfa8ebcbe1f5ba8cd0997c124c492
Joseph Cheek <joseph@cheek.com>
2014-04-24 13:58:03 -0400

    fix st louis coords

--- sites/all/modules/stlouis/stlouis_move.tpl.php
+++ sites/all/modules/stlouis/stlouis_move.tpl.php
@@ -324,7 +324,7 @@ EOF;
 
 <script type="text/javascript">
 
-var map = L.map('leafmap').setView([38.627222, -90.4], 11);
+var map = L.map('leafmap').setView([38.627222, -90.3], 13);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     maxZoom: 18
