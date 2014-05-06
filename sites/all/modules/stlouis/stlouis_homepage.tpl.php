349a451e8364b693d6715713f6431600521d872c
Joseph Cheek <joseph@cheek.com>
2014-05-05 23:33:59 -0400

    [#28] fix

--- sites/all/modules/stlouis/stlouis_homepage.tpl.php
+++ sites/all/modules/stlouis/stlouis_homepage.tpl.php
@@ -756,13 +756,13 @@ EOF;
       <select name="target">
 EOF;
 
-    if ($game_user->fkey_clans_id)
-      echo ('<option value="clan">Clan</option>');
-    
     if (($game_user->can_broadcast_to_party) ||
       ($game_user->fkey_neighborhoods_id == 75))
       echo ('<option value="neighborhood">' . $hood . '</option>');
 
+    if ($game_user->fkey_clans_id)
+      echo ('<option value="clan">Clan</option>');
+    
 // TESTING -- users can party chat but it costs 1 Action -- jwc 11Jan2014
 //    if ($elected_official_type == 2 || $game == 'celestial_glory')
 // if a party official
